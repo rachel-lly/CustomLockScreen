006955b90f7524d71880c3de4d1ec9b184d5f70b
rachel-lly <3556753750@qq.com>
2021-07-25 23:13:38 +0800

    refresh

--- app/src/main/java/com/example/customlockscreen/activity/EditNoteAttributeActivity.kt
+++ app/src/main/java/com/example/customlockscreen/activity/EditNoteAttributeActivity.kt
@@ -1,8 +1,6 @@
 package com.example.customlockscreen.activity
 
-import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.SpannableStringBuilder
