9a528280ddaf9cfbf7c6d08765c89d7c95ff2917
rachel-lly <3556753750@qq.com>
2021-09-07 22:23:29 +0800

    choose picture

--- app/src/main/java/com/example/customlockscreen/fragment/MineFragment.kt
+++ app/src/main/java/com/example/customlockscreen/fragment/MineFragment.kt
@@ -1,19 +1,29 @@
 package com.example.customlockscreen.fragment
 
+import android.Manifest
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.database.Cursor
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.customlockscreen.application.MyApplication
 import com.example.customlockscreen.databinding.FragmentMineBinding
 import com.example.customlockscreen.model.db.DataBase
 import com.example.customlockscreen.model.db.DataViewModel
+import com.example.customlockscreen.util.ToastUtil.Companion.toast
+import com.example.library.PermissionX
 import com.google.android.material.datepicker.MaterialDatePicker
 import java.text.SimpleDateFormat
 import java.util.*
 
+
+const val IMAGE_REQUEST_CODE = 12
 
 class MineFragment : Fragment() {
 
@@ -43,22 +53,63 @@ class MineFragment : Fragment() {
         binding.eventNum.text = labelDao.getAllLabelsName().size.toString()
         binding.sortNoteNum.text = sortNoteDao.getAllSortNotesName().size.toString()
 
+        binding.mineAvater.setOnClickListener {
+            PermissionX.request(activity!!,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.CAMERA){ allgranted, deniedList ->
+
+                if(allgranted){
+                    //跳转系统相册
+                    val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    startActivityForResult(intent, IMAGE_REQUEST_CODE)
+
+                }else{
+                    context!!.toast("你拒绝了 $deniedList")
+                }
+
+            }
+        }
+
         dataViewModel = ViewModelProvider(this).get(DataViewModel::class.java)
 
-        dataViewModel.getAllSortNotesByObserve().observe(this,{
+        dataViewModel.getAllSortNotesByObserve().observe(this, {
             binding.sortNoteNum.text = it.size.toString()
         })
 
-        dataViewModel.getAllLabelsByObserve().observe(this,{
+        dataViewModel.getAllLabelsByObserve().observe(this, {
             binding.eventNum.text = it.size.toString()
         })
 
-        val firstInstallTime = context?.let { MyApplication._context!!.packageManager.getPackageInfo(it.packageName,0).firstInstallTime }
+        val firstInstallTime = context?.let { MyApplication._context!!.packageManager.getPackageInfo(it.packageName, 0).firstInstallTime }
 
         val day = (todayTime- firstInstallTime!!)/(1000*3600*24)
 
         binding.useDayNum.text = day.toString()
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode){
+            IMAGE_REQUEST_CODE -> {
+                if (resultCode == RESULT_OK) {
+                    val selectedImageUri = data!!.data
+                    val filePathColumn = arrayOf(MediaStore.Images.Media.DATA)
+                    val cursor: Cursor = selectedImageUri?.let {
+                        context?.contentResolver?.query(it,
+                                filePathColumn, null, null, null)
+                    }!! //从系统表中查询指定Uri对应的照片
+
+                    cursor.moveToFirst()
+                    val columnIndex: Int = cursor.getColumnIndex(filePathColumn[0])
+                    val path = cursor.getString(columnIndex) //获取照片路径
+
+                    cursor.close()
+                    val bitmap = BitmapFactory.decodeFile(path)
+                    binding.mineAvater.setImageBitmap(bitmap)
+                }
+            }
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
